2530fe6885d301c603e8044a630856c0893548fa
HarryGu <harry.guapi@gmail.com>
2023-01-20 14:00:34 -0600

    add new components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './layouts/default/default.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
