49f29fbb45a8c9c64702a74a006bfa10ebb3c37f
HarryGu <harry.guapi@gmail.com>
2023-01-20 19:35:50 -0600

    add new components

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
+import { DefaultComponent } from './default.component';
+import { PostsComponent } from 'src/app/modules/posts/posts.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav'
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule
   ]
 })
 export class DefaultModule { }
